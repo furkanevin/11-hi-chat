a8024110f6c5572a472c60995bcd2eb0e5457e91
Furkan Evin <furkanevin00@gmail.com>
2024-12-19 23:10:29 +0300

    updaet

--- src/utils/generateColor.js
+++ src/utils/generateColor.js
@@ -1,11 +1,12 @@
 // Kullanıcıya özel renk üreten fonksiyon
-function generateColor(id) {
-  // id'den hash değeri oluştur
+function generateColor(id, name) {
+  // id ve name birleştirilerek hash değeri oluştur
+  const input = `${id}-${name}`;
   let hash = 0;
 
   // Basit bir hash fonksiyonu
-  for (let i = 0; i < id.length; i++) {
-    hash = id.charCodeAt(i) + ((hash << 5) - hash);
+  for (let i = 0; i < input.length; i++) {
+    hash = input.charCodeAt(i) + ((hash << 5) - hash);
     hash = hash & hash; // 32 bit'e indirgenmiş bir hash
   }
 
@@ -13,12 +14,6 @@ function generateColor(id) {
   let r = (hash >> 16) & 0xff;
   let g = (hash >> 8) & 0xff;
   let b = hash & 0xff;
-
-  // Sarı renkten kaçınmak için (r, g yüksek ve b düşük olduğunda)
-  if (r > 200 && g > 200 && b < 100) {
-    b += 100; // Mavi tonunu artır
-    b = b > 255 ? 255 : b; // RGB sınırını aşma
-  }
 
   // Rengi hex formatına dönüştür
   return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
